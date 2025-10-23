aeac920303e5fb50635c26ec11b1b9539b4d172e
n salehi <nimasalehi.auth@icloud.com>
2025-10-23 13:02:34 +0330

    Initial Commit

---
+++ SwiftUI-SculpturesnWarehouse/SwiftUI_SculpturesnWarehouseApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_SculpturesnWarehouseApp.swift
+//  SwiftUI-SculpturesnWarehouse
+//
+//  Created by Nima Salehi on 10/23/25.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_SculpturesnWarehouseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
